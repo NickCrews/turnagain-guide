502742ea49b33db25746376c09bf8494f5062396
Nick Crews <nicholas.b.crews@gmail.com>
2024-12-27 14:56:06 -0900

    remove extra widgets from cesium viewer

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -6,11 +6,21 @@ import { useEffect } from 'react'
 export default function MapStatic() {
   useEffect(() => {
     const viewer = new Viewer('cesiumContainer', {
+      geocoder: false,
+      homeButton: false,
+      sceneModePicker: false,
+      baseLayerPicker: false,
+      navigationHelpButton: false,
+      animation: false,
+      timeline: false,
+      fullscreenButton: false,
+      vrButton: false,
+      infoBox: false,
       // terrainProvider: await createWorldTerrainAsync(),
     })
     return () => {
       viewer.destroy()
     }
   }, [])
-  return <div id="cesiumContainer" style={{height: '100vh', width: '100vw'}} />
+  return <div id="cesiumContainer" style={{ height: '80vh', width: '80vh' }} />
 }

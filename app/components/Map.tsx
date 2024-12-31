72e3aee10e20a35e0471b99ddd11fd134fcee01c
Nick Crews <nicholas.b.crews@gmail.com>
2024-12-31 12:20:43 -0900

    map: start camera in better position, add 3D terrain

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -7,6 +7,9 @@ import {
   UrlTemplateImageryProvider,
   ScreenSpaceEventType,
   ScreenSpaceEventHandler,
+  createWorldTerrainAsync,
+  Ion,
+  Cartesian3,
 } from 'cesium'
 import { useEffect, useState } from 'react'
 
@@ -17,40 +20,19 @@ import 'cesium/Build/Cesium/Widgets/widgets.css';
 interface MapStaticProps {
   items: Item[];
   /*
-  // If false, it will not zoom to anything.
-  // The default is to zoomTo of the geojson.
-  // If you want to zoom to a different object, pass that object
+  // If not provided, will not zoom to anything.
   */
-  zoomTo?: false | any;
+  zoomTo?: any;
   onItemClick?: (item: Item) => void;
 }
 
 export default function MapStatic({ items = [], zoomTo, onItemClick }: MapStaticProps) {
 
-  const [dataSource, setDataSource] = useState<GeoJsonDataSource | null>(null);
-
-  useEffect(() => {
-    async function loadData() {
-      const newDataSource = await GeoJsonDataSource.load({
-        type: "FeatureCollection",
-        features: items,
-      }, {
-        clampToGround: true,
-        credit: "",
-      });
-      setDataSource(newDataSource);
-    }
-    loadData();
-  }, [items]);
-
-  if (zoomTo === undefined) {
-    zoomTo = dataSource;
-  }
-
-  useEffect(() => {
+  async function initViewer() {
+    Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NjQ5YjhjOS01Y2UyLTRhYWEtOGEzZC1hZDZmZWI0NGQ3MTQiLCJpZCI6MjIxMjE4LCJpYXQiOjE3MTgwMDk5MDB9._pdWlrUxdAtIiuq3PXK8HrRNxcoJa6bOjwWE8cQ3J3Y';
     // TS gets mad I don't provide options, but that isn't required:
     // @ts-expect-error https://github.com/CesiumGS/cesium/pull/12400
-    const baseLayer = ImageryLayer.fromProviderAsync(
+    const baseLayer = await ImageryLayer.fromProviderAsync(
       // TileMapServiceImageryProvider.fromUrl(
       //   buildModuleUrl("Assets/Textures/NaturalEarthII"),
       // ),
@@ -69,6 +51,7 @@ export default function MapStatic({ items = [], zoomTo, onItemClick }: MapStatic
         // rectangle: Rectangle.fromDegrees(-149.2939, 60.7024, -148.8208, 60.8538),
       }),
     );
+
     const viewer = new Viewer('cesiumContainer', {
       geocoder: false,
       homeButton: false,
@@ -80,13 +63,71 @@ export default function MapStatic({ items = [], zoomTo, onItemClick }: MapStatic
       fullscreenButton: false,
       vrButton: false,
       infoBox: false,
-      // terrainProvider: await createWorldTerrainAsync(),
       baseLayer: baseLayer,
+      terrainProvider: await createWorldTerrainAsync(),
     });
+    
+    if (zoomTo) {
+      // The camera only moves once the provided object is loaded,
+      // so be careful if you zoomto the GeoJsonDataSource.
+      viewer.zoomTo(zoomTo);
+    } else {
+      // found this manually flying to the camera position
+      // and then printing the camera params (see below)
+      // 
+      // An alternative method is to set the default view rectangle, eg
+      // Camera.DEFAULT_VIEW_RECTANGLE = Rectangle.fromDegrees(-149.2939, 60.7024, -148.8208, 60.8538);
+      // but this defaults to an overhead, north-up view.
+      const homeCameraSettings = {
+        direction: new Cartesian3(
+          0.3278769581030135,
+          -0.2824292802463268,
+          -0.9015156138440734),
+        position: new Cartesian3(
+          -2686319.8596728067,
+          -1594796.5059449861, 
+          5559071.1843723655
+        ),
+        right: new Cartesian3(
+          -0.9004902466772162,
+          0.23589305569999605,
+          -0.36533790100634883
+        ),
+        up: new Cartesian3(
+          -0.3733208490919934,
+          -0.8501982306268276,
+          0.37120683220038514
+        ),
+      }
+      viewer.scene.camera.direction = homeCameraSettings.direction;
+      viewer.scene.camera.position = homeCameraSettings.position;
+      viewer.scene.camera.right = homeCameraSettings.right;
+      viewer.scene.camera.up = homeCameraSettings.up;
 
-    if (dataSource !== null) {
-      viewer.dataSources.add(dataSource);
+      // function printCamera() {
+      //   var camera = viewer.scene.camera;
+      //   var params = {
+      //     position: camera.position.clone(),
+      //     direction: camera.direction.clone(),
+      //     up: camera.up.clone(),
+      //     right: camera.right.clone(),
+      //     transform: camera.transform.clone(),
+      //     frustum: camera.frustum.clone()
+      //   };
+      //   console.log(params);
+      // }
+      // print every second
+      // setInterval(printCamera, 1000);
     }
+
+    const dataSource = await GeoJsonDataSource.load({
+      type: "FeatureCollection",
+      features: items,
+    }, {
+      clampToGround: true,
+      credit: "",
+    });
+    viewer.dataSources.add(dataSource);
 
     if (onItemClick) {
       const itemsById = Object.fromEntries(items.map(item => [item.id, item]));
@@ -100,14 +141,18 @@ export default function MapStatic({ items = [], zoomTo, onItemClick }: MapStatic
         }
       }, ScreenSpaceEventType.LEFT_CLICK);
     }
+    
+    return viewer;
+  }
 
-    if (zoomTo !== false && zoomTo !== null) {
-      viewer.zoomTo(zoomTo);
-    }
+  useEffect(() => {
+    let viewer: Viewer | undefined;
+    initViewer().then(v => { viewer = v; });
 
     return () => {
-      viewer.destroy()
+      viewer?.destroy()
     }
-  }, [dataSource, zoomTo])
+  }, [items, zoomTo])
+  
   return <div id="cesiumContainer" className="h-full w-full" />
 }

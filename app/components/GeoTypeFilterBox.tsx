003642dde2dcbe925ddbd95eb9d8ebd550ab7295
Nick Crews <nicholas.b.crews@gmail.com>
2025-03-09 17:36:31 -0800

    migrate featuretype combo box to new API

---
+++ app/components/GeoTypeFilterBox.tsx
@@ -0,0 +1,27 @@
+import { MultiCombo } from "@/components/ui/multi-combo"
+import { FEATURE_TYPES, FeatureType } from "@/lib/geo-item"
+import { capitalize } from "@/lib/utils"
+
+const toComboItem = (name: FeatureType) => ({
+  value: name,
+  label: capitalize(name),
+  bgColor: 'grey',
+  textColor: 'black',
+})
+
+const ITEMS = Array.from(FEATURE_TYPES).map(toComboItem)
+
+export function GeoTypeComboBox({selected, onSelected}: {selected: Set<FeatureType>, onSelected: (selected: Set<FeatureType>) => void}) {
+  const description = (
+    <span className="mx-2">
+      Feature Type
+    </span>
+  )
+  return <MultiCombo
+    itemOptions={ITEMS}
+    selectedItems={Array.from(selected).map(toComboItem)}
+    onSelected={(items) => onSelected(new Set(items.map(i => i.value)))}
+    descriptionCallback={(items) => description}
+    labelCallback={(items) => <span>Type</span>}
+  />
+}
--- app/components/RouteFilterBar.tsx
+++ app/components/RouteFilterBar.tsx
@@ -4,6 +4,7 @@ import { Input } from '@/components/ui/input'
 import { FeatureType, FEATURE_TYPES } from '@/lib/geo-item'
 import { ATES } from '@/lib/terrain-rating'
 import { AtesComboBox } from './ATES'
+import { GeoTypeComboBox } from './GeoTypeFilterBox'
 
 interface RouteFilterBarProps {
   filters: Filters,
@@ -26,64 +27,11 @@ export default function RouteFilterBar({ filters, setFilters }: RouteFilterBarPr
   return (
     <div className="flex gap-2 p-2 items-center">
       <SearchBar query={filters.query} setQuery={handleSetQuery} />
-      <DropdownToggles title="Type" options={Array.from(FEATURE_TYPES)} selected={filters.types} setSelected={handleSetSelectedTypes} />
+      <GeoTypeComboBox selected={filters.types} onSelected={handleSetSelectedTypes}/>
       <AtesComboBox selected={filters.atesRatings} onSelected={handleSetSelectedAtesRatings}/>
     </div>
   )
 }
-
-interface DropdownTogglesProps<T extends string> {
-  title: string
-  options: T[]
-  selected: Set<T>
-  setSelected: (selected: Set<T>) => void
-}
-
-function DropdownToggles<T extends string>({ title, options, selected, setSelected }: DropdownTogglesProps<T>) {
-  const [open, setOpen] = useState(false)
-
-  function setChecked(option: T, checked: boolean) {
-    const newSelected = new Set(selected)
-    if (checked) {
-      newSelected.add(option)
-    } else {
-      newSelected.delete(option)
-    }
-    setSelected(newSelected)
-  }
-
-  const label = `${title} ${open ? '▲' : '▼'}`
-
-  return (
-    <div className="relative rounded-md border border-gray-300">
-      <button onClick={() => setOpen(!open)} className="py-0.5 px-1">
-        <span className="text-md font-bold">{label}</span>
-      </button>
-      {open && (
-        <div className="absolute top-full left-0 z-10 flex flex-col gap-2 bg-background">
-          {options.map(option => (
-            <Toggle
-              key={option}
-              checked={selected.has(option)}
-              setChecked={(checked) => setChecked(option, checked)}
-              label={option}
-            />
-            ))}
-          </div>
-        )}
-    </div>
-  )
-}
-
-function Toggle({ checked, setChecked, label }: { checked: boolean, setChecked: (checked: boolean) => void, label: string }) {
-  return (
-    <button className="flex gap-2 items-center hover:cursor-pointer" onClick={() => setChecked(!checked)}>
-      <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
-      <label>{label}</label>
-    </button>
-  )
-}
-
 
 function SearchBar({ query, setQuery }: { query: string, setQuery: (query: string) => void }) {
   const [rawQuery, setRawQuery] = useState(query);

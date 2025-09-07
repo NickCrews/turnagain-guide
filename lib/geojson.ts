45ed58611c1e0a268073d359b25fe0317e174df9
Nick Crews <nicholas.b.crews@gmail.com>
2025-09-07 15:19:04 -0600

    generate geojson and gpx files using instrutmentation.ts hook

---
+++ lib/geojson.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath } from "url";
+import { allGeoItems } from "@/routes";
+import { type GeoItem } from "./geo-item";
+import {type FeatureCollection } from 'geojson';
+
+
+export default function geoJSONFromGeoItems(geoItems: readonly GeoItem[]): FeatureCollection {
+  const features = geoItems.map(geoItem => ({
+    type: "Feature" as const,
+    id: geoItem.id,
+    properties: geoItem.properties,
+    geometry: geoItem.geometry,
+  }));
+  const geojson: FeatureCollection = {
+    type: "FeatureCollection",
+    features,
+  };
+  console.log(`Generated GeoJSON with ${features.length} features.`);
+  return geojson;
+}

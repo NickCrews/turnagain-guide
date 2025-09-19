b88e4a7a3a032b36d7c0f010c5deb16f1a04ea88
Nick Crews <nicholas.b.crews@gmail.com>
2025-09-19 09:23:31 -0600

    adjust layout so that /routes/** main content is bounded by the remaining height of the page after the Header, but all other main content is unbounded

---
+++ app/(default)/layout.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function DefaultLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+}

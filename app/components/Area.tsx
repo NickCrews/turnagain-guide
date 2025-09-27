7bd2d0933dbe8577c2cc6fe25cdc32e510a44d36
Nick Crews <nicholas.b.crews@gmail.com>
2025-09-27 11:41:24 -0600

    sort areas in combobox

--- app/components/Area.tsx
+++ app/components/Area.tsx
@@ -4,6 +4,7 @@ import { hashStringToColor } from "@/lib/colors";
 import { MultiCombo } from "@/components/ui/multi-combo"
 import Link from "next/link";
 import { GeoItem } from "@/lib/geo-item";
+import { ValidRouteId } from "@/routes";
 
 function getAreaInfo(areaId: string, items: GeoItem[]): GeoItem {
   const result = items.find((item) => item.id == areaId);
@@ -42,7 +43,9 @@ type ComboItem = {
 };
 
 function useComboItems(): [(areaId: string) => ComboItem, ComboItem[]] {
-  const items = useGeoItems().filter((item) => item.properties.feature_type == 'area');
+  const items = useGeoItems().filter((item) => item.properties.feature_type == 'area').sort((a, b) =>
+    sortByArea(a.id as ValidRouteId, b.id as ValidRouteId)
+  );
   const toComboItem = (areaId: string): ComboItem => {
     const item = getAreaInfo(areaId, items);
     return {
@@ -65,4 +68,34 @@ export function AreaComboBox({selected, onSelected}: {selected: Set<string>, onS
     onSelected={(items) => onSelected(new Set(items.map(i => i.value)))}
     labelCallback={() => <span>Area</span>}
   />
+}
+
+const AREA_ORDER: ValidRouteId[] = [
+  'wolverine-area',
+  'sharkfin-area',
+  'eddies-area',
+  'tincan-area',
+  'sunburst-area',
+  'magnum-area',
+  'cornbiscuit-area',
+  'pastoral-area',
+  'goldpan-area',
+  'lipps-area',
+  'petes-north-area',
+  'petes-south-area',
+]
+
+function sortByArea(id1: ValidRouteId, id2: ValidRouteId) {
+  const index1 = AREA_ORDER.indexOf(id1);
+  const index2 = AREA_ORDER.indexOf(id2);
+  if (index1 === -1 && index2 === -1) {
+    return id1.localeCompare(id2);
+  }
+  if (index1 === -1) {
+    return 1;
+  }
+  if (index2 === -1) {
+    return -1;
+  }
+  return index1 - index2;
 }

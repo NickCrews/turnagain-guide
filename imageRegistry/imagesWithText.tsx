789e5a4b546f01ade16a5ee8ebb782856f86abdf
Nick Crews <nicholas.b.crews@gmail.com>
2025-11-16 21:32:15 -0700

    adjust vars to "as const satisfies ..."

--- imageRegistry/imagesWithText.tsx
+++ imageRegistry/imagesWithText.tsx
@@ -1,7 +1,8 @@
 import { sunburstAndMagnum } from "./imagesWithPaths";
+import { type ImageWithTitleAndDescription } from "@/lib/image";
 
 export const sunburstMagnumImageText = {
-    titl: "Sunburst and Magnum",
+    title: "Sunburst and Magnum",
     description: "Sunburst on the left side and Magnum on the Right.",
-    ...sunburstAndMagnum
-}
+    ...sunburstAndMagnum,
+} as const satisfies ImageWithTitleAndDescription;

ef506b94231acbd1c75d9994659da717a6b9654a
Nick Crews <nicholas.b.crews@gmail.com>
2026-01-01 11:10:11 -0900

    Add more info to coastal parking lot

--- routes/coastal-parking-lot.tsx
+++ routes/coastal-parking-lot.tsx
@@ -1,3 +1,4 @@
+import RouteLink from "@/app/components/route-link";
 import { type GeoItem } from "@/lib/geo-item";
 
 export const geoItem = {
@@ -16,5 +17,23 @@ export const geoItem = {
     elevation: 19,
     images: [],
   },
-  proseJsx: <>{/* Add content here */}</>,
+  proseJsx: <>
+    <p>
+      This is the parking lot at the bottom of Turnagain Pass next to Turnagain Arm,
+      just before the highway starts climbing up into the pass.
+      This is also known as the "Ingram Creek Parking Lot" since that is where
+      Ingram Creek (the main creek in Turnagain Pass) meets the Arm.
+    </p>
+    <p>
+      There are very large pullouts on either side of the highway here.
+      This is a popular spot for snowmachiners to park so they can ride back along
+      the flat swamps of Turnagain Arm towards Placer/Portage.
+    </p>
+    <p>
+      I have heard that people use this to access <RouteLink routeID="wolverine-peak">
+        Wolverine Peak
+      </RouteLink> by connecting through meadows. I haven't done this myself yet, if
+      you have better info, please let me know!
+    </p>
+  </>,
 } as const satisfies GeoItem;
